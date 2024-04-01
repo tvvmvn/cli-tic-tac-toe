6a58466ac1d06736095e6cc5451d8e65b5f202fa
tvvmvn <tvvmvn@gmail.com>
2024-04-01 21:17:26 +0900

    4/1

--- main.c
+++ main.c
@@ -5,106 +5,91 @@
 #include <time.h>
 
 
-int getbingo(char board[9], int lines[8][3]) {
-  for (int i = 0; i < 8; i++) {
-    int a = lines[i][0];
-    int b = lines[i][1];
-    int c = lines[i][2];
-
+int getbingo(char board[3][3]) {
+  // horizontal bingo
+  for (int r = 0; r < 3; r++) {
     if (
-      board[a] != ' '
-      && board[a] == board[b] 
-      && board[b] == board[c]
+      board[r][0] != ' '
+      && board[r][0] == board[r][1]
+      && board[r][1] == board[r][2]
     ) {
-      return board[a];
-    };
+      return board[r][0];
+    }
+  }
+
+  // vertical bingo
+  for (int c = 0; c < 3; c++) {
+    if (
+      board[0][c] != ' '
+      && board[0][c] == board[1][c]
+      && board[1][c] == board[2][c]
+    ) {
+      return board[0][c];
+    }
+  }
+
+  // cross bingo (\)
+  if (
+    board[0][0] != ' '
+    && board[0][0] == board[1][1]
+    && board[1][1] == board[2][2]
+  ) {
+    return board[0][0];
+  }
+
+  // cross bingo (/)
+  if (
+    board[0][2] != ' '
+    && board[0][2] == board[1][1]
+    && board[1][1] == board[2][0]
+  ) {
+    return board[0][2];
   }
 
   return 0;
 }
 
-int get_target(char board[9], int lines[8][3]) {
-  for (int i = 0; i < 8; i++) {
-    int a = lines[i][0];
-    int b = lines[i][1];
-    int c = lines[i][2];
+void drawboard(char board[3][3]) {
+  printf("-----\n");
 
-    if (
-      board[b] != ' '
-      && board[b] == board[c]
-      && board[a] == ' '
-    ) {
-      return a;
-    } else if (
-      board[a] != ' '
-      && board[a] == board[c]
-      && board[b] == ' '
-    ) {
-      return b;
-    } else if (
-      board[a] != ' '
-      && board[a] == board[b]
-      && board[c] == ' '
-    ) {
-      return c;
+  for (int r = 0; r < 3; r++) {
+    for (int c = 0; c < 3; c++) {
+      printf("%c ", board[r][c]);
     }
+
+    printf("\n");
   }
 
-  return -1;
-}
-
-void drawboard(char board[9]) {
-  printf("\n");
-  printf(" %c | %c | %c \n", board[0], board[1], board[2]);
-  printf("---+---+---\n");
-  printf(" %c | %c | %c \n", board[3], board[4], board[5]);
-  printf("---+---+---\n");
-  printf(" %c | %c | %c \n", board[6], board[7], board[8]);
-  printf("\n");
+  printf("-----\n");
 };
 
 void game() {
+  srand(time(NULL));
+
   // constants
   enum Player { USER = 1, COM };
-  enum Symbol { O = 'O', X = 'X' };
-  int lines[8][3] = {
-    {0, 1, 2},
-    {3, 4, 5},
-    {6, 7, 8},
-    {0, 3, 6},
-    {1, 4, 7},
-    {2, 5, 8},
-    {0, 4, 8},
-    {2, 4, 6},
-  };
 
   // variables
-  char board[9];
-
-  for (int i = 0; i < 9; i++) {
-    board[i] = ' ';
-  }
-
   enum Player turn;
   int winner;
   int count = 0;
+  int bingo;
+  char board[3][3];
+
+  for (int r = 0; r < 3; r++) {
+    for (int c = 0; c < 3; c++) {
+      board[r][c] = ' ';
+    }
+  }
 
   // lot 
   turn = (rand() % 2) + 1;
-
-  if (turn == USER) {
-    printf("\nYou first\n");
-  } else {
-    printf("\nCom first\n");
-  }
+  printf("%s First\n", turn == USER ? "You" : "Com");
 
   // playing
   while (1) {
-    // user
     if (turn == USER) {
-      // user input
       int input;
-      int idx;
 
       while (1) { 
         drawboard(board);
@@ -112,98 +97,60 @@ void game() {
         
         scanf("%d", &input);
 
-        idx = input - 1;
+        int x = (input - 1) / 3;
+        int y = (input - 1) % 3;
 
-        if (board[idx] != O && board[idx] != X) {
-          board[idx] = O;
+        if (board[x][y] == ' ') {
+          board[x][y] = 'O';
           count++;
           break;
         } else {
           printf("▶︎ Occupied. try again\n");
         }
       }
-    } 
-    
-    // com
-    if (turn == COM) {
-      int target = get_target(board, lines);
-      int com_idx;
+    } else {
+      int com_idx = 0;
+      
+      while (1) { 
+        com_idx = rand() % 9;
 
-      if (target > -1) {
-        board[target] = X;
-      } else {
-        while (1) { 
-          com_idx = rand() % 9;
+        int x = com_idx / 3;
+        int y = com_idx % 3;
 
-          if (board[com_idx] == ' ') {
-            board[com_idx] = X;
-            break;
-          } 
-        }
+        if (board[x][y] == ' ') {
+          board[x][y] = 'X';
+          break;
+        } 
       }
 
       count++;
-    };
-
-    // check bingo
-    {
-      int bingo = getbingo(board, lines);
-
-      if (bingo == O) {
-        winner = USER;
-        break;
-      }
-      
-      if (bingo == X) { 
-        winner = COM;
-        break;
-      } 
-
-      if (count > 8) {
-        break;
-      }
     }
+    
+    // check result
+    bingo = getbingo(board);
 
-    // change turn
-    turn = (turn == USER) ? COM : USER;
-  }
+    if (bingo) {
+      drawboard(board);
 
-  // end
-  drawboard(board);
+      if (bingo == 'O') {
+        printf("▶︎ YOU WIN!\n");
+      } else {
+        printf("▶︎ YOU LOSE\n");
+      }
 
-  if (winner == USER) {
-    printf("▶︎ YOU WIN\n");
-  } else if (winner == COM) {
-    printf("▶︎ YOU LOSE\n");
-  } else {
-    printf("▶︎ DRAW!\n");
-  }
+      break;
+    } else if (count > 8) {
+      drawboard(board);
 
-  // continue
-  char c;
-
-  printf("▶︎ play again? y/n: ");
-  scanf(" %c", &c);
-
-  if (c == 'y') {
-    game();
-  } else if (c == 'n') {
-    printf("▶︎ ok. bye\n");
-  } else {
-    printf("▶︎ wrong input. terminated\n");
+      printf("▶︎ DRAW\n");
+      break;
+    } else {
+      turn = (turn == USER) ? COM : USER;
+    }
   }
 }
 
 int main() {
-  srand(time(NULL));
-  
-  // intro
-  char start;
-
-  printf("*** TIC TAC TOE ***\n");
-  printf("Press any key to start game: ");
-  scanf(" %c", &start);
-
   game();
 }
 
