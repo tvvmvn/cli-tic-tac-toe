e81aaba90f135b711ed678b16732456e33002ba4
tvvmvn <tvvmvn@gmail.com>
2024-10-17 22:21:48 +0900

    10.17

--- main.c
+++ main.c
@@ -3,46 +3,126 @@
 #include <math.h>
 #include <stdlib.h>
 #include <time.h>
+#define USER 1
+#define COM 2
 
-int getBingo(char board[3][3]) {
-  // horizontal bingo
+int lot();
+int rowCrossed(char board[3][3]);
+int columnCrossed(char board[3][3]);
+int diagonalCrossed(char board[3][3]);
+int isCrossed(char board[3][3]);
+void drawBoard(char board[3][3]);
+
+int main() {
+  srand(time(NULL));
+
+  // variables
+  int whoseTurn;
+  int gameCount = 0;
+  char board[3][3];
+  // initilaize board variables
   for (int r = 0; r < 3; r++) {
-    if (
-      board[r][0] != ' '
-      && board[r][0] == board[r][1]
-      && board[r][1] == board[r][2]
-    ) {
-      return board[r][0];
+    for (int c = 0; c < 3; c++) {
+      board[r][c] = ' ';
     }
   }
 
-  // vertical bingo
+  // lot 
+  whoseTurn = lot();
+  printf("%s First\n", whoseTurn == USER ? "You" : "Com");
+
+  // playing
+  while (!isCrossed(board) && gameCount < 9) {
+    if (whoseTurn == USER) {
+      drawBoard(board);
+
+      int input;
+      printf("▶︎ Choose a number from 1 to 9: ");
+      scanf("%d", &input);
+
+      int r = (input - 1) / 3;
+      int c = (input - 1) % 3;
+
+      if (board[r][c] == ' ') {
+        board[r][c] = 'O';
+      } else {
+        printf("▶︎ Occupied. try again\n");
+        continue;
+      }
+
+      whoseTurn = COM;
+      gameCount++;
+
+    } else {
+      int com_idx = rand() % 9;
+      int r = com_idx / 3;
+      int c = com_idx % 3;
+
+      if (board[r][c] != ' ') {
+        continue;
+      } 
+
+      board[r][c] = 'X';
+      whoseTurn = USER;
+      gameCount++;
+    }
+  }
+
+  // get result
+  drawBoard(board);
+
+  if (!isCrossed(board)) {
+    printf("▶︎ DRAW\n");
+  } else {
+    if (whoseTurn == COM) {
+      printf("▶︎ YOU WIN!\n");
+    } else if (whoseTurn == USER) {
+      printf("▶︎ YOU LOSE\n");
+    } 
+  }
+}
+
+int lot() {
+  return (rand() % 2) + 1;
+}
+
+int rowCrossed(char board[3][3]) {
+  for (int r = 0; r < 3; r++) {
+    if (board[r][0] != ' ' && board[r][0] == board[r][1] && board[r][1] == board[r][2]) {
+      return 1;
+    }
+  }
+
+  return 0;
+}
+
+int columnCrossed(char board[3][3]) {
   for (int c = 0; c < 3; c++) {
-    if (
-      board[0][c] != ' '
-      && board[0][c] == board[1][c]
-      && board[1][c] == board[2][c]
-    ) {
-      return board[0][c];
+    if (board[0][c] != ' ' && board[0][c] == board[1][c] && board[1][c] == board[2][c]) {
+      return 1;
     }
   }
 
+  return 0;
+}
+
+int diagonalCrossed(char board[3][3]) {
   // cross bingo (\)
-  if (
-    board[0][0] != ' '
-    && board[0][0] == board[1][1]
-    && board[1][1] == board[2][2]
-  ) {
-    return board[0][0];
+  if (board[0][0] != ' ' && board[0][0] == board[1][1] && board[1][1] == board[2][2]) {
+    return 1;
   }
 
   // cross bingo (/)
-  if (
-    board[0][2] != ' '
-    && board[0][2] == board[1][1]
-    && board[1][1] == board[2][0]
-  ) {
-    return board[0][2];
+  if (board[0][2] != ' ' && board[0][2] == board[1][1] && board[1][1] == board[2][0]) {
+    return 1;
+  }
+
+  return 0;
+}
+
+int isCrossed(char board[3][3]) {
+  if (rowCrossed(board) || columnCrossed(board) || diagonalCrossed(board)) {
+    return 1;
   }
 
   return 0;
@@ -61,74 +141,3 @@ void drawBoard(char board[3][3]) {
 
   printf("-----\n");
 };
-
-int main() {
-  srand(time(NULL));
-
-  // constants
-  enum Player { USER = 1, COM };
-  // variables
-  enum Player turn;
-  int count = 0;
-  int bingo = 0;
-  char board[3][3];
-  // initilaize board;
-  for (int r = 0; r < 3; r++) {
-    for (int c = 0; c < 3; c++) {
-      board[r][c] = ' ';
-    }
-  }
-
-  // lot 
-  turn = (rand() % 2) + 1;
-  printf("%s First\n", turn == USER ? "You" : "Com");
-
-  // playing
-  while (!bingo && count < 9) {
-    if (turn == USER) {
-      drawBoard(board);
-
-      int input;
-      printf("▶︎ Choose a number from 1 to 9: ");
-      scanf("%d", &input);
-
-      int x = (input - 1) / 3;
-      int y = (input - 1) % 3;
-
-      if (board[x][y] == ' ') {
-        board[x][y] = 'O';
-      } else {
-        printf("▶︎ Occupied. try again\n");
-        continue;
-      }
-    } else {
-      int com_idx = rand() % 9;
-      int x = com_idx / 3;
-      int y = com_idx % 3;
-
-      if (board[x][y] != ' ') {
-        continue;
-      } 
-
-      board[x][y] = 'X';
-    }
-
-    turn = (turn == USER) ? COM : USER;
-    bingo = getBingo(board);
-    count++;
-  }
-
-  // get result
-  drawBoard(board);
-
-  if (bingo == 'O') {
-    printf("▶︎ YOU WIN!\n");
-  } else if (bingo == 'X') {
-    printf("▶︎ YOU LOSE\n");
-  } else {
-    printf("▶︎ DRAW\n");
-  }
-}
-
-
-
